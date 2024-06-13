9aead21add92d88d8f8e50e48677d6d54fcac921
microegor <microegor321@gmail.com>
2024-06-13 14:46:37 +0200

    feat: Добавлены файлы поекта

---
+++ ArgumentParser.cs
@@ -0,0 +1,21 @@
+class ArgumentParser
+{
+    public static string GetString(string[] args, string name)
+    {
+        for (int i = 0; i < args.Length; i++)
+        {
+            var item = args[i];
+            if (item == name)
+            {
+                return args[i + 1];
+            }
+        }
+        throw new Exception($"Argument '{name}' not found");
+    }
+
+    public static int GetInt32(string[] args, string name)
+    {
+        var value = GetString(args, name);
+        return Convert.ToInt32(value);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿
+class Program
+{
+    public static int Main(string[] args)
+    {
+        var inFile = ArgumentParser.GetString(args, "--in");
+        Console.WriteLine($"--in: {inFile}");
+        var outFile = ArgumentParser.GetString(args, "--out");
+        Console.WriteLine($"--out: {outFile}");
+
+        var csv = File.ReadAllLines(inFile);
+
+        for (int i = 0; i < csv.Length; i++)
+        {
+            var item = csv[i];
+            item = item.Replace(",", "|");
+            Console.WriteLine($"|{item}|");
+
+            if (i == 0)
+            {
+                var colSize = item.Split("|").Count();
+                for (int j = 0; j < colSize; j++)
+                {
+                    Console.Write("|---");
+                }
+                Console.WriteLine("|");
+            }
+        }
+
+
+        return 0;
+    }
+}
